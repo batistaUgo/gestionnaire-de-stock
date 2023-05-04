0de8c0bdcf505ffb4adde8ede55d159e49d030a3
ugobat <107422411+batistaUgo@users.noreply.github.com>
2023-05-04 09:42:37 +0200

    test

--- login.php
+++ login.php
@@ -5,18 +5,22 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>gestionnaire de stock</title>
+    <link rel="stylesheet" href="css.css">
 </head>
 <body>
 
     <form action="index.php" method="post">
-        <label for="">nom</label>
-        <input type="text" name="nom" placeholder="identifiant" required>
-        <br>
-        <label for="">mot de passe</label>
-        <input type="password" name="mdp" placeholder="********" required>
-        <br>
-        <input type="submit" value="valider" name="submit">
-        <input type="reset" value="annuler">
+        <div class="wrapper">
+                <form class="form-signin">
+                <h2 class="form-signin-heading">Connecter vous</h2>
+                <input type="text" class="form-control" name="username" placeholder="Email Address" required="" autofocus="" />
+                <input type="password" class="form-control" name="password" placeholder="Password" required=""/>      
+                <label class="checkbox">
+                    <input type="checkbox" value="remember-me" id="rememberMe" name="rememberMe"> Se souvenir de moi
+                </label>
+                <button class="btn btn-lg btn-primary btn-block" type="submit">Login</button>
+            </form>
+        </div>
     </form>
     
 </body>
